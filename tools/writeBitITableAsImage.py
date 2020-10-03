7bf2c0c4c3e436410d852245c9d84a12242fd9f3
GishaidBinim <binim.gishaid@outlook.co.il>
2020-10-04 01:35:14 +0300

    first works v of gaame of life

---
+++ tools/writeBitITableAsImage.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+import copy 
+import random
+
+length, hight = 250, 250
+
+
+  
+currentMetrix = [ [ 0 for i in range(length) ] for j in range(hight) ]
+preMetrix = [ [ 0 for i in range(length) ] for j in range(hight) ]
+
+#preMetrix[5][5] = 1
+#preMetrix[5][6] = 1
+#preMetrix[5][7] = 1
+#preMetrix[4][5] = 1
+#preMetrix[5][6] = 1
+#preMetrix[4][7] = 1
+#preMetrix[5][5] = 1
+#preMetrix[4][4] = 1
+#preMetrix[4][7] = 1
+#preMetrix[4][3] = 1
+#preMetrix[5][6] = 1
+#preMetrix[3][6] = 1
+
+
+
+
+image = np.zeros((hight,length,3), np.uint8)
+#image[5,5] = (200,200,200)
+#image[5,6] = (200,200,200)
+#image[5,7] = (200,200,200)
+
+for i in range(2, len(preMetrix)-2):
+        for j in range(2, len(preMetrix[i])-2):
+                preMetrix[i][j] = random.choice([0, 1])
+                if preMetrix[i][j] == 1:
+                    image[i,j] = (200,200,200)
+
+cv2.imshow('image',image)
+cv2.waitKey(500)
+
+for x in range(0, 400):
+    for i in range(2, len(preMetrix)-2):
+        for j in range(2, len(preMetrix[i])-2):
+            num = 0
+            if(preMetrix[i-1][j-1]) == 1:
+                num = num +1
+            if(preMetrix[i-1][j]) == 1:
+                num = num +1
+            if(preMetrix[i-1][j+1]) == 1:
+               num = num +1
+            if(preMetrix[i][j-1]) == 1:
+               num = num +1
+            if(preMetrix[i][j+1]) == 1:
+               num = num +1
+            if(preMetrix[i+1][j-1]) == 1:
+               num = num +1
+            if(preMetrix[i+1][j]) == 1:
+               num = num +1
+            if(preMetrix[i+1][j+1]) == 1:
+               num = num +1
+
+            currentMetrix[i][j] = 0
+            if((preMetrix[i][j]) == 1 and num == 2) or (preMetrix[i][j] == 0 and num == 3):
+                currentMetrix[i][j] = 1
+                image[i,j] = (200,200,200)
+            else:
+                image[i,j] = (0,0,0)
+
+    preMetrix = copy.deepcopy(currentMetrix)
+
+    cv2.imshow('image',image)
+    cv2.waitKey(2)
+
+
