e67c2caad82db1ef1251b8fdbf29baf28a81f083
GishaidBinim <binim.gishaid@outlook.co.il>
2020-10-04 16:53:47 +0300

    ennance alg

--- tools/writeBitITableAsImage.py
+++ tools/writeBitITableAsImage.py
@@ -3,60 +3,95 @@ import numpy as np
 import copy 
 import random
 
-length, hight = 250, 250
+def getLifeValue(i,j, matrix):
+    if i < 0 or j < 0 or i == length or j == length:
+        return 0
+    return matrix[i][j]
 
 
+length, hight = 400, 400
   
 currentMetrix = [ [ 0 for i in range(length) ] for j in range(hight) ]
 preMetrix = [ [ 0 for i in range(length) ] for j in range(hight) ]
 
-
-
 image = np.zeros((hight,length,3), np.uint8)
 
 
-for i in range(2, len(preMetrix)-2):
-        for j in range(2, len(preMetrix[i])-2):
+for i in range(0, len(preMetrix)):
+        for j in range(0, len(preMetrix[i])):
                 preMetrix[i][j] = random.choice([0, 1])
                 if preMetrix[i][j] == 1:
-                    image[i,j] = (200,200,200)
+                    image[i,j] = (150,30,200)
+
+for i in range(0, len(preMetrix)):
+        for j in range(0, len(preMetrix[i])):
+                life = random.choice([0, 1])
+                if life == 0:
+                    preMetrix[i][j] = 0
+                    image[i,j] = (0,0,0)
+
+for i in range(0, len(preMetrix)):
+        for j in range(0, len(preMetrix[i])):
+                life = random.choice([0, 1])
+                if life == 0:
+                    preMetrix[i][j] = 0
+                    image[i,j] = (0,0,0)
+
 
 cv2.imshow('image',image)
 cv2.waitKey(500)
 
-for x in range(0, 400):
-    for i in range(2, len(preMetrix)-2):
-        for j in range(2, len(preMetrix[i])-2):
+for x in range(0, 50):
+    for i in range(1, len(preMetrix)-1):
+        for j in range(1, len(preMetrix[i])-1):
             num = 0
             if(preMetrix[i-1][j-1]) == 1:
-                num = num +1
+               num = num +1
             if(preMetrix[i-1][j]) == 1:
-                num = num +1
+                num = num +1   
             if(preMetrix[i-1][j+1]) == 1:
-               num = num +1
+                num = num +1
             if(preMetrix[i][j-1]) == 1:
-               num = num +1
+                num = num +1      
             if(preMetrix[i][j+1]) == 1:
-               num = num +1
+                num = num +1   
             if(preMetrix[i+1][j-1]) == 1:
-               num = num +1
+                num = num +1
             if(preMetrix[i+1][j]) == 1:
-               num = num +1
+                num = num +1     
             if(preMetrix[i+1][j+1]) == 1:
-               num = num +1
+                num = num +1
 
             currentMetrix[i][j] = 0
-            if((preMetrix[i][j]) == 1 and num == 2) or (preMetrix[i][j] == 0 and num == 3):
+            if((preMetrix[i][j]) == 1 and num == 2) or ((preMetrix[i][j]) == 0 and num == 3):
                 currentMetrix[i][j] = 1
-                image[i,j] = (200,20,200)
+                image[i,j] = (150,30,200)            
             else:
                 image[i,j] = (0,0,0)
+
+    for i in range(0, len(preMetrix)):
+        for j in range(0, len(preMetrix[i])):
+            if i == 0 or i == length-1 or j == 0 or j == length -1: 
+                num = 0
+                num = num + getLifeValue(i-1,j-1, preMetrix)
+                num = num + getLifeValue(i-1,j, preMetrix)
+                num = num + getLifeValue(i-1,j+1, preMetrix)
+                num = num + getLifeValue(i,j-1, preMetrix)
+                num = num + getLifeValue(i,j+1, preMetrix)
+                num = num + getLifeValue(i+1,j-1, preMetrix)
+                num = num + getLifeValue(i+1,j, preMetrix)
+                num = num + getLifeValue(i+1,j+1, preMetrix)
+
+                currentMetrix[i][j] = 0
+                if((preMetrix[i][j]) == 1 and num == 2) or ((preMetrix[i][j]) == 0 and num == 3):
+                    currentMetrix[i][j] = 1
+                    image[i,j] = (150,30,200)            
+                else:
+                    image[i,j] = (0,0,0)
 
     preMetrix = copy.deepcopy(currentMetrix)
 
     cv2.imshow('image',image)
-    cv2.waitKey(2)
-
-    #just wanna see how to publish new branch
+    cv2.waitKey(100)
 
 
